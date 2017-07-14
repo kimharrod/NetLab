346ff3cf5b916855b7e7a4bb62361439cf6d4a30
Kimberly Harrod <kim@fios.com>
2017-07-14 11:01:35 -0500

    Render Main component

---
+++ app/app.js
@@ -0,0 +1,9 @@
+// Include the React dependencies
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+// Include the Main component
+var Main = require("./components/Main");
+
+// Code to render Main component
+ReactDOM.render(<Main />, document.getElementbyId("app"));
