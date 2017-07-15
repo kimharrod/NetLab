f871428d932bacaa3cd0e8dba0e4230a3c6b9779
Kimberly Harrod <kim@fios.com>
2017-07-14 23:08:13 -0500

    Set up react router

--- app/app.js
+++ app/app.js
@@ -1,9 +1,33 @@
-// Include the React dependencies
-var React = require("react");
-var ReactDOM = require("react-dom");
+// Import React dependencies
+import React from 'react'
+import { render } from 'react-dom'
+import { Router, Route, browserHistory} from 'react-router'
 
-// Include the Main component
-var Main = require("./components/Main");
+// Import home component and children components
+import Home from './children/home'
+import Menu from './children/menu'
+import Viewer from './children/viewer'
+import Search from './children/search'
+import Newnote from './children/newnote'
+import Bookmarks from './children/bookmarks'
+import Profile from './children/profile'
 
-// Code to render Main component
-ReactDOM.render(<Main />, document.getElementbyId("app"));
+
+// Set up react router
+render((
+	<Router history={browserHistory}>
+		<Route path="/" component={Home}>
+
+			{/* make the following components children of "Home" */}
+			<Route path="/menu" component={Menu}/>
+			<Route path="/search" component={Search}/>
+	      	<Route path="/newnote" component={Newnote}/>
+	      	<Route path="/bookmarks" component={Bookmarks}/>
+	      	<Route path="/profile" component={Profile}/>
+			
+			{/* route to pass the slideNum parameter to the child */}
+			<Route path="/viewer/:slideNum" component={ Viewer }/>
+
+		</Route>
+	</Router>
+), document.getElementById('app'))
