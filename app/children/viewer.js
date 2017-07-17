2be6fe8bcbacaab907b5771b4536e4747e0f2de9
Kimberly Harrod <kim@fios.com>
2017-07-16 23:07:15 -0500

    Viewer component

---
+++ app/children/viewer.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+export default React.createClass({
+
+// function to save the coordinates of last slide view when exiting viewer component
+componentWillUnmount: function () {
+
+	console.log("unmount slideNum: " + this.props.params.slideNum);
+	// cross-frame access to variable in the third-party (IIPMOO) viewer
+	var coords = $('#scopeView').get(0).contentWindow.viewCoords;
+	console.log("coords: " + coords);
+	// save the last slide view to local storage
+	localStorage.setItem("lastview", coords);
+
+},
+
+	
+  	// assemble and render the iframe for the viewer component
+  	render() {
+
+  		// make slide number available to viewer component
+  		var num = this.props.params.slideNum;
+  		console.log("render slideNum: " + num);
+
+  		// point to the image url on the slide server
+  		var url = 'http://netlab.fios.vc/iipn/index.html?image=' + num + '.tif';
+
+  		// detect direct navigation from viewer link in the navbar
+  		if (num === "direct") {
+
+  			// assign url to reflect last slide view
+  			url = localStorage.getItem("lastview");
+
+  		}
+
+  		// or display the slide selected from menu
+	   	return <div><iframe id="scopeView" src={url} height="600" width="100%"></iframe>
+		</div>
+  }
+})
