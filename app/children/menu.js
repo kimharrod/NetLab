25755354c971b19b424be66eaffb797284f27362
Kimberly Harrod <kim@fios.com>
2017-07-22 21:50:20 -0500

    Menu hierarchy

--- app/children/menu.js
+++ app/children/menu.js
@@ -17,34 +17,82 @@ componentDidMount: function () {
 	// this completes before menu is displayed
 	helpers.getSlides().then(function(data) {
 
-	// assemble the menu for display and applying hyperlinks to each menu item
-		var slideSet = Object.keys(data).map(function(s) {
-			var viewpath = '/viewer/' + data[s].number + '/' + data[s].structure + '/' + data[s].system;
+		// assemble the menu for display and apply hyperlinks to each menu item
+		// start by creating an array to receive the names of all the systems
+		var systems = [];
+		// iterate through all the slides and push the system names into the systems array
+		var sysSet = Object.keys(data).map(function(s){ 
 
-				return (
-					<li key={data[s].number} ><Link to={viewpath}>System: {data[s].system}<br/>
-					Structure: {data[s].structure} </Link><br/>
-					</li>
-				)
-			});
+			// if the system name isn't already in the array
+			if (systems.indexOf(data[s].system) < 0) {
 
-			this.setState({ slides: slideSet});
+				systems.push(data[s].system);
+			}
+	  		
+		}); 
 
-		}.bind(this));
-	},
+		// create an array of objects containing the hierarchy of systems & slides
+		var slides = [];
+		var catSet = [];
+
+		for (var i=0; i < systems.length; i++) {
+			var cat = {};
+			
+			catSet = [];
+    		var slideArr = Object.keys(data).map(function(s){ 
+  
+    			if (systems[i] === data[s].system) {
+  					var slideObj = {};
+    				slideObj.number = data[s].number;
+    				slideObj.system = data[s].system;
+    				slideObj.structure = data[s].structure;
+    				catSet.push(slideObj);
+				} // end if	slide matches system
+      		
+    		}); // for each slide
+
+    		cat.system = systems[i];
+    		cat.list = catSet;  		
+    		slides.push(cat);
+
+    	} // end for each system	
+
+        this.setState({ slides: slides });
+
+      }.bind(this));
+
+},
+
 
 	render() {
 
-		return (
-			<div>
-				<ul>
+		var slidelist = this.state.slides;
+	    return ( <div>
+	        {
+	            slidelist.map((item, index) => {
+	            	
+	            	return (
+	              		<div key={index}>
+	                	<h4 >{item.system}</h4>
+	               		{
+	                		item.list.map((subitem, i) => {
+	                			
+	                			var viewpath = '/viewer/' + subitem.number + '/' + subitem.structure + '/' + subitem.system;
 
-					{this.state.slides}
+	                  			return (
+	                     			<ul ><Link to={viewpath}><li>{subitem.structure}</li></Link></ul>
+	                  			)
+	                		})
+	               		}
+	              		</div>
+	            	)
+	          	})
+	        }           
+	          
+	        </div>
 
-				</ul>
-			</div>
-		)
+	    ) // end return		      
 
-	}
+    } // end render
 
-})
+}) // end create class
