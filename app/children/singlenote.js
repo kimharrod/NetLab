727a2edf667e9946d551b4fd4cba9cb333c5112d
Kimberly Harrod <kim@fios.com>
2017-07-26 17:17:20 -0500

    Edit notes

--- app/children/singlenote.js
+++ app/children/singlenote.js
@@ -18,12 +18,59 @@ getInitialState: function() {
   },
 
 
+editNote: function(e){
+
+  if (this.state.note.authorId === localStorage.getItem("userId")) {
+
+    e.preventDefault();
+    console.log("in editNote"); 
+    var noteText = $("#noteBod").text();
+    console.log("the text of the note is: " + noteText);
+    $("#noteBod").html('<textarea rows="2" cols="50" type="text" size="43" id="ntext">'); 
+    $("#ntext").val(noteText);
+        $('#ntext').on('keydown',function(ev){
+          if(ev.which == '13'){
+           var noteId = localStorage.getItem("key");
+           console.log("noteId: " + noteId)
+          this.firebaseRef = Firebase.database().ref("histology");
+           this.firebaseRef.child('/notes/' + noteId).update({body: $("#ntext").val()});
+          $("#noteBod").text($("#ntext").val());
+          } 
+        });
+    }
+},
+
+
+editSubject: function(event){
+
+  if (this.state.note.authorId === localStorage.getItem("userId")) {
+
+    event.preventDefault();
+    console.log("in editNote"); 
+    var noteSubj = $("#noteSubj").text();
+    console.log("the text of the note is: " + noteSubj);
+    $("#noteSubj").html('<input type="text" size="43" id="nsubj" value="' + noteSubj + '">'); 
+        $('#nsubj').on('keydown',function(evnt){
+          if(evnt.which == '13'){
+           var noteId = localStorage.getItem("key");
+           console.log("noteId: " + noteId)
+          this.firebaseRef = Firebase.database().ref("histology");
+           this.firebaseRef.child('/notes/' + noteId).update({subject: $("#nsubj").val()});
+          $("#noteSubj").text($("#nsubj").val());
+          } 
+        });
+   }     
+
+},
+
 componentWillMount() {
     // set up Firebase connection & get reference to Histology branch
 	  // allows data to be fetched
     this.firebaseRef = Firebase.database().ref("histology");
     // assign the note's node key to oneNote
     var oneNote = this.props.params.key;
+    // put the node ID in local storage
+    localStorage.setItem("key", oneNote);
     // create an object to receive the note content
     var currentnote = {};
     // do a snapshot of that single user note
@@ -82,12 +129,11 @@ deleteNote() {
           <div className="panel-body">
             
               <p className="lead">
-                Author: {author}<br/>
-                System: {system}<br/>
-                Structure: {structure}<button onClick={this.deleteNote} data-key={key} type="button" className="btn btn-muted btn-primary pull-right" data-toggle="tooltip" title="Delete Note">&#215;</button><br/>
-                Subject: {subject}<br/>
-                Note: {body}
-              </p>
+                System: {system}<br/>
+                Structure: {structure}<button id="deleteNote" onClick={this.deleteNote} data-key={key} type="button" className="btn btn-muted btn-primary pull-right" data-toggle="tooltip" title="Delete Note">&#215;</button><br/>
+                <span onClick={this.editSubject}>Subject:</span> <span id="noteSubj">{subject}</span><br/>
+                <span onClick={this.editNote}>Note:</span> <span id="noteBod">{body}</span>
+              </p>
 
           </div>
         </div>
